15a603bd68ad02d43489c1874bdfd186600b2af4
Ryan <RR.Schweitzer@gmail.com>
2015-01-09 15:02:07 -0700

    Finished

--- practice.js
+++ practice.js
@@ -105,11 +105,16 @@ alert(newMyName);
 //Create a function called outerFn which returns an anonymous function which returns your name.
 
   var outerFn = function() {
-
-  }
+  	return function() {
+  		return "Ryan";
+  	};
+  };
+ 
 
 //Now save the result of invoking outerFn into a variable called innerFn.
 
-  //Code Here
+var innerFn = outerFn();
 
 //Now invoke innerFn.
+
+innerFn();
